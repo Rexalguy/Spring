e6ca1bdab0fe8f29ba0a598353ab540fc2fbcd82
Aburek Emmanuel <aburekemmanuel@gmail.com>
2026-02-24 16:17:40 +0300

    Transaction setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO account (id, name, balance) VALUES (1, 'Alice', 1000);
+INSERT INTO account (id, name, balance) VALUES (2, 'Bob', 1000)
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE table account (
+  id int primary key auto_increment,
+  name varchar(255) not null,
+  amount double not null
+);
